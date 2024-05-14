a77de07712ebc475b3bc76ef10f7181a9089b245
pinchal <pinchal@ggwp.me>
2024-05-14 22:28:33 +0200

    Creation of the base project

---
+++ tydomio/__init__.py
@@ -0,0 +1,7 @@
+"""TydomIO.
+
+This package provides a set of utilities and functions to interact with
+a Delta Dore Tydom.
+
+The main entry point for this package is the `AsyncTydomClient` class.
+"""
